a385a9ede9ec0508286dd9cedb7330ed441b9464
Dylan Moberg <dylanmoberg@LexBlogs-MacBook-Pro-4.local>
2016-07-09 15:38:20 -0700

    test named iffies and their functionalities

--- scopesAndClosures.js
+++ scopesAndClosures.js
@@ -1,3 +1,15 @@
 function thisIsAClosure() {
-    
+    var b = 2;
+    var c = 4;
+    (function bar() {
+        if (c === 0) {
+            return;
+        }
+        console.log(b,c);
+        c --;
+        bar();
+    })();
+
 }
+
+thisIsAClosure();
